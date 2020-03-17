5d4d33c6d8590b88a220d578380dba5acc43090a
CristianPuga <58120121+CristianPuga@users.noreply.github.com>
2020-03-17 08:25:39 +0100

    .gitignores

--- GestampPrueba2Back/Controllers/Personas3Controller.cs
+++ GestampPrueba2Back/Controllers/Personas3Controller.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using GestampPrueba2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestampPrueba2.Controllers
 {
